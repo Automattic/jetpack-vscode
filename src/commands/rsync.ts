2278ef459009d6d3bebf5b23815a1efca419d0f0
Kuba Birecki <kuba.birecki@crystalplanet.io>
2023-10-13 21:45:42 +0200

    Update the rsync command to accept a remote dir argument and run 'jetpack rsync'

--- src/commands/rsync.ts
+++ src/commands/rsync.ts
@@ -1,22 +1,43 @@
+import {execSync} from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import {findJetpackRoot} from '../util';
 
-const getPlugins = () => {
+const getPlugins = (rootPath: string) =>
+    fs.readdirSync(path.join(rootPath, 'projects', 'plugins'), {withFileTypes: true})
+        .filter(result => result.isDirectory())
+        .map(result => result.name);
+
+export const rsyncCommand = async () => {
     const jetpackRoot = findJetpackRoot();
 
     if (!jetpackRoot) {
-        return [];
+        return;
     }
 
-    return fs.readdirSync(path.join(jetpackRoot, 'projects', 'plugins'), {withFileTypes: true})
-        .filter(result => result.isDirectory())
-        .map(result => result.name);
-};
-
-export const rsyncCommand = async () => {
-    const plugin = await vscode.window.showQuickPick(getPlugins(), {
+    const plugin = await vscode.window.showQuickPick(getPlugins(jetpackRoot), {
         placeHolder: "Which plugin would you like to sync?",
     });
+
+    const wpPath = await vscode.window.showInputBox({
+        prompt: 'Enter the remote path to upload the plugin contents to.',
+        placeHolder: 'user@server:public_html/wp-content/plugins/jetpack',
+    });
+
+    const confirmation = await vscode.window.showWarningMessage(
+        `You're about to upload the contents of plugins/${plugin} to ${wpPath}. Do you want to proceed?`,
+        'Yes',
+        'No'
+    );
+
+    if (confirmation !== 'Yes') {
+        return;
+    }
+
+    try {
+        execSync(`pnpm jetpack rsync ${plugin} ${wpPath}`, {cwd: jetpackRoot});
+    } catch (error) {
+        console.log(error);
+    }
 };
