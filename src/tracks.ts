b7ab5d0eb6f42589e49f9014fc32a61b4722de0d
sdixon194 <steve.dixon@automattic.com>
2023-11-02 12:43:35 -0400

    Add setting to enable/disable analytics

--- src/tracks.ts
+++ src/tracks.ts
@@ -3,9 +3,19 @@
  * 
  * curl 'https://public-api.wordpress.com/rest/v1.1/tracks/record?http_envelope=1' -H 'Accept-Encoding: gzip, deflate' -H 'User-Agent: cURL, baby!' -H 'Content-Type: application/json' -H 'Accept: application/json' --data '{"commonProps": {"_ul": "myname"}, "events": [{"_en": "wpcom_test_test"}]}' --compressed
  */
+import * as vscode from 'vscode';
 import { exec } from 'child_process';
 
+
 export function callTrackEvent( $eventName: string ) {
+
+	// If analytic tracking is disabled, bail.
+	const config = vscode.workspace.getConfiguration('jetpackVSCode');
+	const enableAnalytics = config.get('enableAnalytics');
+	if (!enableAnalytics) {
+		return;
+	}
+
 	const url = 'https://public-api.wordpress.com/rest/v1.1/tracks/record?http_envelope=1';
 	const headers = {
 		'Accept-Encoding': 'gzip, deflate',
